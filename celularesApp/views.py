b1f6361eb89f61dce6b3fd27200025bb16608792
LuisRivera330 <coartac.luis.rivera@gmail.com>
2023-03-14 22:13:41 -0500

    Cambio

--- celularesApp/views.py
+++ celularesApp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
- return HttpResponse("PROYECTO CREADO CORRECTAMETE :)")
+ return HttpResponse("PROYECTO CREADO CORRECTAMETE LUIS :)")
